00e4d4f87836dd48bcd68ea19ed2a72d3b20d1c6
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-24 22:23:17 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,9 +3,9 @@ class HauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
-      t.integer :price
-      t.text :image_url
-      t.text :size
+      t.text :location
+      t.string :theme
+      t.float :price
       t.timestamp
     end
   end
